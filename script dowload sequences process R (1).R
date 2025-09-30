71663d2c8e3e39b75097e53f1414bd126657ad8d
Analorena Cifuentes <analorena@vt.edu>
2025-09-30 10:54:55 -0400

    Update script dowload sequences process R (1).R

--- script dowload sequences process R (1).R
+++ script dowload sequences process R (1).R
@@ -1,23 +1,35 @@
-install.packages("rentrez")
 library(rentrez)
 
 # API KEY VT
 set_entrez_key("27f406d7b920408fd5147371a4c2147dc008")
 
 # FILTRING
-query <- "Rabies virus[Organism] AND Glyphonycteris[Host] AND complete genome[Title] AND 2011:2024[Publication Date]"
-search_results <- entrez_search(db = "nucleotide", term = query, retmax = 20)
- 
+query <- "Rabies virus[Organism] AND bat[Host] AND complete genome[Title] AND 2011:2024[Publication Date]"
+search_count <- entrez_search(db = "nucleotide", term = query, retmax = 0)
+total_results <- search_count$count
+print(paste("total sequences:", total_results))
+
+#Download IDs
+search_results <- entrez_search(db = "nucleotide", term = query, retmax = total_results)
+
+
+
 # SHOW RESULTS
 print(search_results$count)
+print(search_results$ids)
 
 # SHOW IDs OF SEQUENCES
 print(search_results$ids)
 
 # DOWLOAD SEQUENCES FASTA
 fasta_data <- entrez_fetch(db = "nucleotide", id = search_results$ids, rettype = "fasta", retmode = "text")
+fasta_data <- entrez_fetch(db = "nucleotide",
+                           id = search_results$ids,
+                           rettype = "fasta",
+                           retmode = "text")
 
 # SAVE SEQUENCES IN A FILE
 writeLines(fasta_data, "rabies_Lasiurus_USA_complete_genomes.fasta")
 
-
+# Save
+writeLines(fasta_data, "C:/Users/analorena/Desktop/rabies_Lasiurus_USA_complete_genomes.fasta")
